89bcdee877e10f8d23f09cdf46f812c6ab513bda
Gabriel Solomon <hello@gabrielsolomon.ro>
2018-01-22 17:53:33 +0200

    update traits

--- src/View.php
+++ src/View.php
@@ -2,6 +2,7 @@
 
 namespace Nip\View;
 
+use Nip\View\Traits\HasDataTrait;
 use Nip\View\Traits\HasHelpersTrait;
 
 /**
@@ -11,11 +12,11 @@ use Nip\View\Traits\HasHelpersTrait;
  */
 class View implements ViewInterface
 {
+    use HasDataTrait;
     use HasHelpersTrait;
 
     protected $request = null;
 
-    protected $data = [];
     protected $blocks = [];
     protected $basePath = null;
 
@@ -33,89 +34,6 @@ class View implements ViewInterface
         }
 
         return null;
-    }
-
-    /**
-     * @param $name
-     * @return mixed|null
-     */
-    public function __get($name)
-    {
-        return $this->get($name);
-    }
-
-    /**
-     * @param $name
-     * @param $value
-     * @return View
-     */
-    public function __set($name, $value)
-    {
-        return $this->set($name, $value);
-    }
-
-    /**
-     * @param  string $name
-     * @return mixed|null
-     */
-    public function get($name)
-    {
-        if ($this->has($name)) {
-            return $this->data[$name];
-        } else {
-            return null;
-        }
-    }
-
-    /**
-     * @param string $name
-     * @return bool
-     */
-    public function has($name)
-    {
-        return isset($this->data[$name]);
-    }
-
-    /**
-     * @param string $name
-     * @param mixed $value
-     * @return $this
-     */
-    public function set($name, $value)
-    {
-        $this->data[$name] = $value;
-
-        return $this;
-    }
-
-    /**
-     * @param $name
-     * @return bool
-     */
-    public function __isset($name)
-    {
-        return isset($this->data[$name]);
-    }
-
-    /**
-     * @param $name
-     */
-    public function __unset($name)
-    {
-        unset($this->data[$name]);
-    }
-
-    /**
-     * @param string $name
-     * @param string $appended
-     * @return View
-     */
-    public function append($name, $appended)
-    {
-        $value = $this->has($name) ? $this->get($name) : '';
-        $value .= $appended;
-
-        return $this->set($name, $value);
     }
 
     /**
