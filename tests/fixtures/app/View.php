4060eb0bf575cb12d6786a2277354432cea22561
Gabriel Solomon <hello@gabrielsolomon.ro>
2018-02-02 13:58:04 +0200

    fix tests

--- tests/fixtures/app/View.php
+++ tests/fixtures/app/View.php
@@ -8,16 +8,5 @@ namespace Nip\View\Tests\Fixtures\App;
  */
 class View extends \Nip\View
 {
-    /**
-     * Builds path for including
-     * If $view starts with / the path will be relative to the root of the views folder.
-     * Otherwise to caller file location.
-     *
-     * @param string $view
-     * @return string
-     */
-    protected function buildPath($view)
-    {
-        return parent::buildPath($view);
-    }
+
 }
